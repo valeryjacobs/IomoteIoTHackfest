38f294b04d16e2331eabd0c93f6c8e3637387b9c
Valery Jacobs <valeryjacobs@hotmail.com>
2017-03-21 11:41:34 +0100

    Init

---
+++ DeviceSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeviceSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
