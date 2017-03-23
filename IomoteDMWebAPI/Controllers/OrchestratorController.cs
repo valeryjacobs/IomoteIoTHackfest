f63e864785e2ddb29d62365fb322d304a4fc8f0c
Guenda Sciancalepore <guscianc@microsoft.com>
2017-03-23 17:27:23 +0100

    orchestrator api

--- IomoteDMWebAPI/Controllers/OrchestratorController.cs
+++ IomoteDMWebAPI/Controllers/OrchestratorController.cs
@@ -4,21 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
 
 namespace IomoteDMWebAPI.Controllers
 {
     public class OrchestratorController : ApiController
     {
-        // GET: api/Orchestrator
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
+		static string connectionString = ConfigurationManager.ConnectionStrings["IoTHub_Conn"].ToString();
+		// GET: api/Orchestrator
+		//public IEnumerable<string> Get()
+		//{
+		//    return new string[] { "value1", "value2" };
+		//}
 
-        // GET: api/Orchestrator/5
-        public string Get(string id)
+		// GET: api/Orchestrator/5
+		public async Task<string> Get(string id)
         {
-            return "handling the '" + id + "' command.";
+			var serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+			var commandMessage = new Microsoft.Azure.Devices.Message(Encoding.ASCII.GetBytes(id));
+
+			await serviceClient.SendAsync("myFirstDevice", commandMessage);
+			return "handling the '" + id + "' command.";
         }
 
         //// POST: api/Orchestrator
